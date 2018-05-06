1cb9c6e638a7c05d08b548487529454d0622c104
deuzene <deuzene@zaclys.net>
2018-05-06 09:01:49 +0200

    correction format

--- test.pl
+++ test.pl
@@ -6,4 +6,20 @@ use Smart::Comments;
 use Data::Dumper;
 use feature ":5.24";
 
+use Text::LineEditor ;
 
+my $text = line_editor() ;
+# print $text ;
+
+format STDOUT=
+^<<<<<<<<<<<<<<
+$text
+^<<<<<<<<<<<<<<
+$text
+^<<<<<<<<<<<<<<
+$text
+^<<<<<<<<<<<<<<
+$text
+.
+
+write ;
